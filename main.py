e71b66a14cafc4ba451eda31f5fd4709d3c37584
Trevor Ramdhan <tramdhan@users.noreply.github.com>
2023-12-29 17:24:52 -0500

    render dynamic data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +14,30 @@ with col2:
         Hi, I'm Trevor Ramdhan, a full-stack Node-js, Python developer.
     """
     st.info(content)
+
+content2 = """
+Below is a list of projects and apps I have developed.
+"""
+
+st.write(content2)
+
+#  col sizes, with 0.5 space between the data cols
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+df = pandas.read_csv("data/data.csv", sep=";")
+
+#  first 0 to 10 items
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("assets/images/" + row["image"])
+        st.write(f"[Source Code]({row["url"]})")
+
+#  second set, from 10 to 20 items
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("assets/images/" + row["image"])
+        st.write(f"[Source Code]({row["url"]})")
