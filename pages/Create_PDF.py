3e5e5e60f965ac17019bd3406ebfc477b0b16f9f
Trevor Ramdhan <tramdhan@users.noreply.github.com>
2023-12-30 21:33:29 -0500

    read xlsx files

--- pages/Create_PDF.py
+++ pages/Create_PDF.py
@@ -17,14 +17,17 @@ h=12, recommended to set to font-size
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=14)
-    pdf.set_text_color(0,179,0)
+    pdf.set_text_color(0, 179, 0)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(12,27,200,27)  # start and end positions of line
+
+    #  create lines down the page, starting at pos 20 from top of page, ends at 290, 10 line spacing
+    for y in range(20, 290, 10):
+        pdf.line(10, y, 200, y)  # line is 200 in length, 10 from left of page
 
     # set footer, 240 below text from top of page
     pdf.ln(240)
     pdf.set_font(family="Times", style="I", size=8)
-    pdf.set_text_color(180,180,180)
+    pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     #  for each Topic above, create a num of blank pages per the "Pages" equal to Pages value in CSV
@@ -36,5 +39,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        #  create lines down the page, 20 char from top of page
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)  # line is 200 in length, 10 from left of page
 
 pdf.output("output.pdf")
