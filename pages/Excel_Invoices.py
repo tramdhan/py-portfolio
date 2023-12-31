c66f3cecf654a106b4985aa45bf626dd4443237f
Trevor Ramdhan <tramdhan@users.noreply.github.com>
2023-12-30 21:45:08 -0500

    create PDF for each invoice with title

--- pages/Excel_Invoices.py
+++ pages/Excel_Invoices.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("../data/invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="Letter")
+    pdf.add_page()
+    #  abstract filename without extension
+    filename = Path(filepath).stem
+    invoice_num = filename.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.set_text_color(0, 179, 0)
+    pdf.cell(w=50, h=8, txt=f"Invoice #: {invoice_num}", align="L", ln=1, border=0)
+    pdf.output(f"../PDFs/{invoice_num}.pdf")
